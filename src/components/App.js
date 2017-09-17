cbbd59df62deb4a738839df9dcb3b14ae865d2d3
njia95 <nathan.jia@mail.utoronto.ca>
2017-09-17 14:35:48 -0400

    set up infra

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Categories from './Categories';
+import Category from './Category';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <Route exact path='/' render={() => (
+                    <Categories />
+                )} />
+                <Route path='/:category/posts' render={() => (
+                    <Category />
+                )} />
+            </div>
+        );
+    }
+}
+
+export default App;
