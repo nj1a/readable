2b54884dcd647d1a3e42fe6dbd854424881b8608
njia95 <nathan.jia@mail.utoronto.ca>
2017-12-01 20:49:46 -0500

    removed delete comment container

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -8,7 +8,6 @@ import EditPostPage from './EditPostPage'
 import AddPostPage from './AddPostPage'
 import AddCommentPage from './AddCommentPage'
 import EditCommentPage from './EditCommentPage'
-import DeleteCommentPage from './DeleteCommentPage'
 import NoMatch from '../components/NoMatch'
 
 const Root = () => (
@@ -19,7 +18,6 @@ const Root = () => (
             <Route exact path='/posts/:id/edit' component={EditPostPage} />
             <Route exact path='/posts/:parentId/comments/add' component={AddCommentPage} />
             <Route exact path='/comments/:id/edit' component={EditCommentPage} />
-            <Route exact path='/comments/:id/delete' component={DeleteCommentPage} />
             <Route exact path='/:category' component={App} />
             <Route exact path='/:category/:id' component={PostPage} />
             <Route component={NoMatch} />
